e0fc9b4d9b006d7cf7d8ed6b5cdfc0610841760c
joshbae119 <mclceo119@gmail.com>
2024-12-10 10:54:13 +0900

    20241210.서비스 로직구현

---
+++ Board/src/main/java/com/springboot/biz/question/QuestionService.java
@@ -0,0 +1,21 @@
+package com.springboot.biz.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+	
+	private final QuestionRepository questionRepository;
+	
+	public List<Question> getList(){
+		return this.questionRepository.findAll();
+	}
+	
+	
+
+}
