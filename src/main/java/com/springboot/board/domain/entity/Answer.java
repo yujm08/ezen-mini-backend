01ba786a37740f7e6a025e9de7373f8cb80d0482
ChulhoBAE <mclceo119@gmail.com>
2025-01-01 22:47:57 +0900

    sweager 빌드성공

--- src/main/java/com/springboot/board/domain/entity/Answer.java
+++ src/main/java/com/springboot/board/domain/entity/Answer.java
@@ -2,8 +2,6 @@ package com.springboot.board.domain.entity;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.CreatedDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
