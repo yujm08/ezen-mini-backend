7d0ce4ee876f1e5817a677ee38a5549f683ee413
ChulhoBAE <mclceo119@gmail.com>
2025-01-03 00:08:28 +0900

    Update SwaggerConfig: Change contact name to '깃허브소스코드 바로가기' for improved clarity in API documentation

--- src/main/java/com/springboot/board/config/SwaggerConfig.java
+++ src/main/java/com/springboot/board/config/SwaggerConfig.java
@@ -28,7 +28,7 @@ public class SwaggerConfig {
                                 .version("v2.0.0")
                                 .description("Spring Boot Board REST API 문서")
                                 .contact(new Contact()
-                                                .name("깃허브소스코드")
+                                                .name("깃허브소스코드 바로가기")
                                                 // .email("stable.luckychu@gmail.com")
                                                 .url("https://github.com/joshbae119/ezen-mini-backend.git"));
                 // .license(new License()
