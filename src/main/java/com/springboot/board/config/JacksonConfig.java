245fe6ed1d97d47715d83be13f1e34a57a5ce838
ChulhoBAE <mclceo119@gmail.com>
2025-01-05 14:23:20 +0900

    V2.3.0 Jackson 파일 날짜표기 한국시간으로 변경

--- src/main/java/com/springboot/board/config/JacksonConfig.java
+++ src/main/java/com/springboot/board/config/JacksonConfig.java
@@ -6,15 +6,18 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import java.util.TimeZone;
 
 @Configuration
 public class JacksonConfig {
-    
+
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        objectMapper.registerModule(javaTimeModule);
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
         return objectMapper;
     }
 
@@ -22,4 +25,4 @@ public class JacksonConfig {
     public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
         return new MappingJackson2HttpMessageConverter(objectMapper);
     }
-} 
+}
